57718767b59c6d6d2d8724b574fcca30790e93d7
donutwizard666 <thebeardedwarrior@icloud.com>
2020-05-08 14:52:27 -0700

    finish ex4 FINALLY got three problems done WOOOOOOOOOOOOOOOOOOO

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,20 @@
 def has_negatives(a):
+    result = []
+    my_dict = {}
 
-    """
-    YOUR CODE HERE
-    """
+    # for every number in a
+    for number in a:
+        # as long as number is less than 0
+        if number < 0:
+            # add all negative numbers to the dict
+            my_dict[-number] = number
+    # iterating again
+    for number in a:
+        # seeing if number is already in the dict
+        if number in my_dict:
+            # if it is then appending it to the result array
+            result.append(number)
+
 
     return result
 
